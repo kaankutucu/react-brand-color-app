9a303c47881bd94f5591472d054af2cbf10cd6e0
Kaan <49414441+kaankutucu@users.noreply.github.com>
2022-04-14 12:05:11 +0300

    content color added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
+import MainContext from "./MainContext";
+import BrandsData from "./brands.json";
+import {useState} from 'react';
+import './App.css';
+
 
 function App() {
-  return (
-    <>
-      <Sidebar/>
-      <Content/>
-    </>
-  );
+
+
+    const brandsArray = []
+    Object.keys(BrandsData).map(key => {
+        brandsArray.push(BrandsData[key])
+    })
+
+    const [brands, setBrands] = useState(brandsArray)
+    const [selectedBrands, setSelectedBrands] = useState([])
+
+    const data = {
+        brands,
+        setSelectedBrands,
+        selectedBrands
+    }
+    return (
+        <>
+            <MainContext.Provider value={data}>
+                <Sidebar/>
+                <Content/>
+            </MainContext.Provider>
+        </>
+    );
 }
 
 export default App;
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import Search from "./Search";
 
-function Content(props) {
+import Search from "./Search";
+import Brand from "./Brand";
+import MainContext from "../MainContext";
+import {useContext} from "react";
+
+
+function Content() {
+
+  const {brands} = useContext(MainContext)
+
+
     return (
         <main className="content">
-           <header className="header">
+            <header className="header">
                 <Search/>
-           </header>
+            </header>
+
+            <section className="brands">
+                {
+                    brands.map(brand => (
+                        <Brand brand={brand}/>
+                    ))
+                }
+            </section>
         </main>
     );
 }
