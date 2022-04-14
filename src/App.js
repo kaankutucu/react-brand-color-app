20b604ea7ab9851436d94178545e47c21b4a15cf
Kaan <49414441+kaankutucu@users.noreply.github.com>
2022-04-14 06:05:22 +0300

    search button added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Sidebar/>
-    </div>
+      <Content/>
+    </>
   );
 }
 
